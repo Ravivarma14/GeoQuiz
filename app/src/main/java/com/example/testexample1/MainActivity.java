9c6d4813b4d24b8280198cb3503f57a0ce05a816
Ravivarma <ravi.varma.madasu.2@gmail.com>
2023-12-28 19:21:07 +0530

    initial code setup

--- app/src/main/java/com/example/testexample1/MainActivity.java
+++ app/src/main/java/com/example/testexample1/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.testexample1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView mTextview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
